0838e5f94b33564b2ecd5df2cd07c5c6064b938e
Austin Robertson <162934591+arobertson7@users.noreply.github.com>
2026-06-21 17:50:55 -0400

    'Add To Library' button added and styled. functionality not yet implemented

--- src/BookCard.tsx
+++ src/BookCard.tsx
@@ -8,15 +8,20 @@ interface Book {
   cover_url: string;
 }
 
-function BookCard({ book }: { book: Book }) {
+interface BookCardProps {
+  book: Book;
+  displayMode?: string; // "userLibrary" or "searchBooks"
+}
+
+function BookCard({ book, displayMode }: BookCardProps) {
 
   return (
     <div className="book-card">
       <div className="book-cover-wrapper">
-        <img 
-          className="book-cover" 
-          src={book.cover_url} 
-          alt={`book cover for ${book.title} by ${book.author}`} 
+        <img
+          className="book-cover"
+          src={book.cover_url}
+          alt={`book cover for ${book.title} by ${book.author}`}
         />
         <div className="book-spine-crease"></div>
       </div>
@@ -25,6 +30,11 @@ function BookCard({ book }: { book: Book }) {
           {book.title} <span className="book-year">({book.year_published})</span>
         </h3>
         <p className="book-author">by {book.author}</p>
+        {displayMode === "searchBooks" && (
+          <button className="book-card-button" type="button">
+            + Add to Library
+          </button>
+        )}
       </div>
     </div>
   )
