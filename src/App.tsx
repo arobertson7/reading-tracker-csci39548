79746ded73706def0798abb081b1d09b25b73dfd
Austin Robertson <162934591+arobertson7@users.noreply.github.com>
2026-06-26 22:31:08 -0400

    created custom hook useLibraryBooks in separate file for bundling all userLibraryBook state and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,12 @@ import SearchBar from './SearchBar';
 import { type OpenLibraryBook } from './OpenLibraryBook';
 import noBookCover from './assets/no-cover.png';
 import { getFilterEmptyStateInfo, getAverageBookRating } from './helpers.tsx'
+import { useLibraryBooks } from './hooks/useLibraryBooks.tsx';
 
-const STORAGE_KEY = "user_library_books";
 const OPEN_LIBRARY_URL = "https://openlibrary.org/search.json?q="; // simply append the query to the end
 
-function App() {
-  const [userLibraryBooks, setUserLibraryBooks] = useState<UserLibraryBook[]>(() => {
-    // Load user's library if it exists in local storage
-    const raw_library_books = localStorage.getItem(STORAGE_KEY);
-    if (!raw_library_books) return [];
-    try {
-      return JSON.parse(raw_library_books) as UserLibraryBook[];
-    } catch {
-      return [];
-    }
-  });
+export default function App() {
+  const { userLibraryBooks, inUserLibrary, removeFromUserLibrary, toggleAddToUserLibrary, updateBookReadingStatus, updateBookRating} = useLibraryBooks();
 
   const [searchResultBooks, setSearchResultBooks] = useState<Book[]>([]);
   const [fetchingSearchResults, setFetchingSearchResults] = useState<boolean>(false);
@@ -51,14 +42,20 @@ function App() {
     }
   }, [darkMode]);
 
-  // Save user's library to localStorage when there is a change
-  useEffect(() => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(userLibraryBooks));
-  }, [userLibraryBooks]);
 
-  const filteredLibraryBooks = userLibraryBooks.filter((book) => (
-    activeFilter === "All" || book.readingStatus === activeFilter.toLowerCase().split(' ').join('-')
-  ));
+  const filteredAndSortedLibraryBooks = userLibraryBooks
+    .filter((book) => (
+      activeFilter === "All" || book.readingStatus === activeFilter.toLowerCase().split(' ').join('-')
+    ))
+    .toSorted((a: UserLibraryBook, b: UserLibraryBook) => {
+      let valA = (sortBy === "Date Added") ? a.dateAdded : (sortBy === "Title" ? a.title.toLowerCase() : a.author.toLowerCase())
+      let valB = (sortBy === "Date Added") ? b.dateAdded : (sortBy === "Title" ? b.title.toLowerCase() : b.author.toLowerCase())
+
+      if (valA < valB) return sortOrder === 'asc' ? -1 : 1;
+      if (valA > valB) return sortOrder === 'asc' ? 1 : -1;
+
+      return 0;
+    });
 
   async function fetchSearchResults(query: string) {
     setHasSearched(true);
@@ -90,59 +87,6 @@ function App() {
       setFetchingSearchResults(false);
       setMostRecentSearch(query);
     }
-  }
-
-  function inUserLibrary(bookId: string) {
-    for (const userLibraryBook of userLibraryBooks) {
-      if (userLibraryBook.id === bookId) return true;
-    }
-    return false;
-  }
-
-  function toggleAddToUserLibrary(book: Book) {
-    if (inUserLibrary(book.id)) { // remove from library
-      removeFromUserLibrary(book);
-    } else { // else add to library
-      addToUserLibrary(book);
-    }
-  }
-
-  function addToUserLibrary(book: Book) {
-    setUserLibraryBooks([...userLibraryBooks, {
-      id: book.id,
-      title: book.title,
-      author: book.author,
-      year_published: book.year_published,
-      cover_url: book.cover_url,
-      readingStatus: 'to-read',
-      dateAdded: new Date()
-    }]);
-  }
-
-  function removeFromUserLibrary(book: Book) {
-    setUserLibraryBooks(userLibraryBooks.filter((userLibraryBook) => userLibraryBook.id !== book.id));
-  }
-
-  function updateBookReadingStatus(bookId: string, newReadingStatus: string) {
-    if (newReadingStatus !== 'to-read' && newReadingStatus !== 'reading' && newReadingStatus !== 'finished') {
-      return; // double check that newReadingStatus is a valid ReadingStatus before changing
-    }
-    setUserLibraryBooks((prevBooks) =>
-      prevBooks.map((book) =>
-        book.id === bookId ? {
-          ...book, readingStatus: newReadingStatus as ReadingStatus,
-          rating: undefined // any change in reading status would logically reset the rating
-        } : book
-      )
-    )
-  }
-
-  function updateBookRating(bookId: string, rating: number) {
-    setUserLibraryBooks((prevBooks) =>
-      prevBooks.map((book) =>
-        book.id === bookId ? { ...book, rating: rating as BookRating } : book
-      )
-    )
   }
 
   return (
@@ -314,7 +258,7 @@ function App() {
                   <span>🔍</span> Search Books
                 </button>
               </div>
-            ) : filteredLibraryBooks.length === 0 ? (
+            ) : filteredAndSortedLibraryBooks.length === 0 ? (
               (() => {
                 const filterInfo = getFilterEmptyStateInfo(activeFilter);
                 return (
@@ -339,25 +283,15 @@ function App() {
               })()
             ) : (
               <div className="grid grid-cols-[repeat(auto-fill,minmax(160px,1fr))] gap-6 w-full pt-2 md:grid-cols-[repeat(auto-fill,minmax(180px,1fr))] md:gap-8">
-                {filteredLibraryBooks
-                  .toSorted((a: UserLibraryBook, b: UserLibraryBook) => {
-                    let valA = (sortBy === "Date Added") ? a.dateAdded : (sortBy === "Title" ? a.title.toLowerCase() : a.author.toLowerCase())
-                    let valB = (sortBy === "Date Added") ? b.dateAdded : (sortBy === "Title" ? b.title.toLowerCase() : b.author.toLowerCase())
-
-                    if (valA < valB) return sortOrder === 'asc' ? -1 : 1;
-                    if (valA > valB) return sortOrder === 'asc' ? 1 : -1;
-
-                    return 0;
-                  })
-                  .map((book) => (
-                    <UserLibraryBookCard
-                      key={book.id}
-                      book={book}
-                      onRemoveFromLibrary={removeFromUserLibrary}
-                      onChangeReadingStatus={updateBookReadingStatus}
-                      onChangeRating={updateBookRating}
-                    />
-                  ))}
+                {filteredAndSortedLibraryBooks.map((book) => (
+                  <UserLibraryBookCard
+                    key={book.id}
+                    book={book}
+                    onRemoveFromLibrary={removeFromUserLibrary}
+                    onChangeReadingStatus={updateBookReadingStatus}
+                    onChangeRating={updateBookRating}
+                  />
+                ))}
               </div>
             )
           )}
@@ -433,5 +367,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
